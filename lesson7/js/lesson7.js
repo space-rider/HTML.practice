d1fda5140da113e790912c3803b2a18dde6533ed
space-rider <sailorbeavis@gmail.com>
2019-01-04 12:41:48 +0200

    Правки и добавления

---
+++ lesson7/js/lesson7.js
@@ -0,0 +1,10 @@
+const firstButton = document.getElementById('first_card');
+const secondButton = document.getElementById('second_card');
+
+firstButton.onclick = function () {
+    window.open('info_page.html');
+}
+
+secondButton.onclick = function () {
+    window.open('first_task.html');
+}
